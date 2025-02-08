2a1712eb0eef67f3b257450ccf024aa3e19d43bd
Gonzalo <gonzafernandez94@gmail.com>
2025-02-07 22:56:37 -0300

    create UserController UserRestController and User

---
+++ src/main/java/com/gonzalo/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.gonzalo.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gonzalo.curso.springboot.webapp.springboot_web.models.User;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details(Model model){
+        
+        User user = new User("Gonza", "Fernandez");
+
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("user", user);
+        
+        return "details";
+    }
+}
