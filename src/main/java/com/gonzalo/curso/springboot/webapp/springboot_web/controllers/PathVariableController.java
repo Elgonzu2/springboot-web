d6d8acaa61bd933a0e554e931180e78731dfbe86
Gonzalo <gonzafernandez94@gmail.com>
2025-02-20 20:27:12 -0300

    ValuesConfig, values and HomeController added, SpringBootWEbApplication, PathVariableController and application.properties modified

--- src/main/java/com/gonzalo/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/gonzalo/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,8 +1,12 @@
 package com.gonzalo.curso.springboot.webapp.springboot_web.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +21,36 @@ import com.gonzalo.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+    
+    // @Value("${config.message}")
+    // private String message;
+    
+    @Value("${config.listOfValues}")
+    private List<String> listOfValues;
+    
+    @Value("${config.code}")
+    private Integer code;
+    
+    @Value("#{ '${config.listOfValues}'.toUpperCase().split(',') }")
+    private List<String> valueList;
+
+    @Value("#{ '${config.listOfValues}'.toUpperCase() }")
+    private String valueString;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, String> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -40,5 +74,25 @@ public class PathVariableController {
         // hacer algo con el usuario save en la bbdd
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+        Long code2 = environment.getProperty("config.code", Long.class);
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("code2", code2);
+        json.put("listOfValues", listOfValues);
+        json.put("valueList", valueList);
+        json.put("valueString", valueString);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
+        json.put("code", code);
+        
+        return json;
     }
 }
